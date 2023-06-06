93f5759cb202eb654125d28d13db4f94e1e37eb5
hmoa01 <hmoa01@gmail.com>
2023-06-06 15:47:48 +0200

    add new post

---
+++ backend/controllers/postController/addPost.js
@@ -0,0 +1,26 @@
+const PostModel = require("../../models/PostModel");
+
+const addPost = async (req, res) => {
+  const { _id } = req.locals;
+  const { title, body, image, tags } = req.body;
+
+  let newPost = {
+    title,
+    body,
+    image,
+    tags,
+    userId: _id,
+  };
+
+  let post = new PostModel(newPost);
+  post
+    .save()
+    .then((post) => {
+      res.send(post);
+    })
+    .catch((error) => {
+      res.send(error.mes);
+    });
+};
+
+module.exports = addPost;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,16 @@
 const { Router } = require("express");
+const verifyToken = require("../middleware/verifyToken");
 
 const router = new Router();
 
+// GET
 router.get("/all", require("../controllers/postController/allPosts"));
+
+//POST
+router.post(
+  "/add",
+  verifyToken,
+  require("../controllers/postController/addPost")
+);
 
 module.exports = router;
