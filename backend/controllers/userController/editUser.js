f3ed09a0249a272b08edab8aaeb099ac4ac9d3e0
hmoa01e <hmoa01@gmail.com>
2023-06-19 13:29:22 +0200

    edit user

--- backend/controllers/userController/editUser.js
+++ backend/controllers/userController/editUser.js
@@ -1,5 +1,23 @@
+const userModel = require("../../models/userModel");
 const editUser = (req, res) => {
-  console.log(req.locals);
+  const user = req.locals;
+  const {userId} = req.params;
+  const {role, email, createdAt, updatedAt, ...editedData} = req.body;
+  let query;
+
+  if(user.role === "admin") {
+    editedData.role = role;
+  } else if (userId !== user._id && user.role !== "admin") {
+    return res.send({msg: "You don't have permission!!!"})
+  }
+
+  userModel.findOneAndUpdate({_id: userId},editedData,{new:true, projection:{password:0}})
+      .then(user => {
+          res.send(user)
+      }).catch(error =>{
+        res.send({error:error.message});
+      })
+
 };
 
 module.exports = editUser;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,13 @@ const { Router } = require("express");
 const verifyToken = require("../middleware/verifyToken");
 const router = new Router();
 
+//GET
 router.get("/", require("../controllers/userController/getAllUsers"));
 router.get("/:uid", require("../controllers/userController/singleUser"))
+
+//PUT
 router.put(
-  "/edit/:uid",
+  "/edit/:userId",
   verifyToken,
   require("../controllers/userController/editUser")
 );
